0dcaab7f03bd85c72abc286e284302cd5778357c
Yevhen <yevhen.kuraiev@gmail.com>
2017-06-18 17:17:26 +0300

    Add files via upload

---
+++ unusual menu.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('nav > ul > li').hover(
+        function () {
+            var hidden = $(this).children('.hidden');
+
+            hidden.children().stop(true);
+
+            hidden.show();
+            hidden.children().filter(':even').animate({ marginLeft: -15 }, 200);
+            hidden.children().filter(':odd').animate({ marginLeft: 15 }, 200);
+        },
+        function () {
+            var hidden = $(this).children('.hidden');
+
+            hidden.hide();
+            hidden.children().filter(':even').animate({ marginLeft: 0 }, 200);
+            hidden.children().filter(':odd').animate({ marginLeft: 0 }, 200);
+        });
+});
